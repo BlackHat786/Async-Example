a529ee3d5b3323690d718334a13d2c394547a35a
Mohammed Ameen Abdool <21612147@dut4life.ac.za>
2019-07-24 22:07:10 +0200

    Add project files.

---
+++ AsyncExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncExample
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Work();
+            Done();
+            Today();
+            Console.ReadKey();
+        }
+
+        static void Work()
+        {
+            Console.Write("Welcome ");
+        }
+
+        static async Task Done()
+        {
+            await Task.Delay(1000); //Creates a task that completes after a specified number of milliseconds.
+            Console.Write("To Async Programming");
+        }
+
+        static void Today()
+        {
+            Console.Write(" Class, ");
+        }
+    }
+}
